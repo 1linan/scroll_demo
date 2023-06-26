0b9b75ebb3c3f8ce216f914d2fe64f11b3a8f4cf
1linan <2591088659@qq.com>
2023-06-27 00:58:38 +0800

    add:触底分堆加载

--- src/views/data.tsx
+++ src/views/data.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from './data.module.less';
 
 export function Data() {
+  const wrapperRef: any = useRef(null);
   const [data, setData] = useState<string[]>([]);
-  const [timer, setTimer] = useState<any[]>([]);
 
 
   //分堆：将数据分成一堆50个,便于观察
@@ -30,31 +30,78 @@ export function Data() {
       }
     }
     //获取分堆后的数组
-    let mixtureArr = groupsFn(mixture);
+    let mixtureArr: string[][] = groupsFn(mixture);
 
-    //分堆定时渲染
-    for (let i = 0; i < mixtureArr.length; i++) {
-      //相当于创建很多定时任务去处理
-      let timer = setTimeout(() => {
-        console.log(i)
-        setData((x: string[]) => [...x, ...mixtureArr[i]]);//赋值渲染
-      }, 6000 * i)
-      setTimer((x) => [...x, timer]);
+    //分堆触底加载
+    let Index = 0;
+    function loadData(mixtureArr: string[][]) {
+      if (Index < mixtureArr.length - 1) {
+        console.log(Index)
+        setData((x) => [...x, ...mixtureArr[Index - 1]]);
+        Index++;
+      }
     }
-  }, []);
 
+    loadData(mixtureArr);
 
-  function onStopRender() {
-    for (let i = 0; i < timer.length; i++) {
-      clearTimeout(timer[i]);
+    function scrollDown() {
+      let wrapper = wrapperRef.current;
+      if (wrapper) {
+        const scrollHeight = wrapper.scrollHeight;
+        const scrollTop = wrapper.scrollTop;
+        const clientHeight = wrapper.clientHeight;
+
+        if (scrollTop + clientHeight >= scrollHeight - 10) {
+          loadData(mixtureArr);
+        }
+      }
     }
-  }
+
+
+    function scrollUp() {
+
+    }
+
+    function scrollFunc(e: any) {
+      let el = e || window.event;
+      el.stopPropagation();
+      if (el.wheelDelta) {
+        if (el.wheelDelta > 0) {
+          //当鼠标滚轮向上滚动时
+          scrollUp();
+        }
+        if (el.wheelDelta < 0) {
+          //当鼠标滚轮向下滚动时
+          scrollDown();
+        }
+      } else if (e.detail) {
+        if (el.detail < 0) {
+          //当鼠标滚轮向上滚动时
+          scrollUp();
+        }
+        if (el.detail > 0) {
+          //当鼠标滚轮向下滚动时
+          scrollDown();
+        }
+      }
+    }
+    // 给页面绑定鼠标滚轮事件,针对火狐的非标准事件
+    window.addEventListener("DOMMouseScroll", scrollFunc); // 给页面绑定鼠标滚轮事件，针对Google，mousewheel非标准事件已被弃用，请使用 wheel事件代替
+    window.addEventListener("wheel", scrollFunc); // ie不支持wheel事件，若一定要兼容，可使用mousewheel
+    window.addEventListener("mousewheel", scrollFunc);
+
+  }, [wrapperRef]);
+
+
+
+
+
+
   return (
     <div className={styles.dataContainer}>
       <div className={styles.dataItems}>
         <h1>数字+字母</h1>
-        <h1><button onClick={onStopRender}>停止渲染</button></h1>
-        <div>
+        <div className={styles.items} ref={wrapperRef}>
           {
             data.map((v, index) => <button key={index}>{v}</button>)
           }
